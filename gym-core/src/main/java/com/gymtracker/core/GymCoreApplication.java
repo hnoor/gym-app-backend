4b21ccec8608595c059c6212937334c7a4ebb86a
H <haider_noor@live.com>
2025-03-25 22:02:08 +0200

    gym core init

---
+++ gym-core/src/main/java/com/gymtracker/core/GymCoreApplication.java
@@ -0,0 +1,12 @@
+package com.gymtracker.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymCoreApplication.class, args);
+    }
+}
